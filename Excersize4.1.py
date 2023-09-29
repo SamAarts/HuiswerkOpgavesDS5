8a2336cf0ff226173fd1ab70c2e7757b4b30f977
SamAarts <505660@student.fontys.nl>
2023-09-29 14:53:26 +0200

    0614376787

--- Excersize4.1.py
+++ Excersize4.1.py
@@ -2,27 +2,31 @@
 import pandas as pindas
 
 
-def 
-project4dataframe = pindas.read_excel('dataProject4.xlsx',sheet_name='20000-211000')
+def Excersize4_1(excelname, sheetname):
+    project4dataframe = pindas.read_excel(excelname,sheet_name=sheetname)
 
-project4dataframe.drop_duplicates(inplace=True)
-project4dataframe.drop(index=[0,1,2,3], inplace=True)
-project4dataframe.columns =['Klant nummer',
-                            'Postcode','CRM',
-                            'Menu','Product',
-                            'Merk','Inhoud',
-                            '2018','2019',
-                            '2020','2021',
-                            '2022']
-df = project4dataframe.astype({'Klant nummer':'int',
-                               'Inhoud':'float',
-                               '2018':'float',
-                               '2019':'float',
-                               '2020':'float',
-                               '2021':'float',
-                               '2022':'float'})
-project4dataframe = df
+    project4dataframe.drop_duplicates(inplace=True)
+    project4dataframe.drop(index=[0,1,2,3], inplace=True)
+    project4dataframe.columns =['Klant nummer',
+                                'Postcode','CRM',
+                                'Menu','Product',
+                                'Merk','Inhoud',
+                                '2018','2019',
+                                '2020','2021',
+                                '2022']
+    df = project4dataframe.astype({'Klant nummer':'int',
+                                'Inhoud':'float',
+                                '2018':'float',
+                                '2019':'float',
+                                '2020':'float',
+                                '2021':'float',
+                                '2022':'float'})
+    project4dataframe = df
 
-project4dataframe = project4dataframe[project4dataframe['CRM'] != 'Result']
-project4dataframe.reset_index(inplace=True, drop=True)
+    project4dataframe = project4dataframe[project4dataframe['CRM'] != 'Result']
+    project4dataframe.reset_index(inplace=True, drop=True)
+    return project4dataframe
+
+
+print(Excersize4_1('dataProject4.xlsx', '20000-211000'))
 
