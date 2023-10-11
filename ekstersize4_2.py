a00e371d73c6769429f48340214527071c1b0eef
Spelfoudt <508773@student.fontys.n>
2023-10-11 10:10:06 +0200

    last minute changes

--- ekstersize4_2.py
+++ ekstersize4_2.py
@@ -1,10 +1,29 @@
 # ronde 2
 import pandas as pindas
+import datetime
+import numpy as np
 
 hotel_trivago = pindas.read_excel('hotelBookings.xlsx',sheet_name='hotel_bookings')
-
-hotel_trivago.dropna(axis='columns',inplace=True)
-hotel_trivago.dropna(axis='rows',inplace=True)
 hotel_trivago.drop_duplicates(inplace=True)
-print(hotel_trivago)
+    
 
+def fix_year():
+    for i in range(len(hotel_trivago)):
+        hotel_trivago.at[i, 'arrival_date_year'] = 2015
+
+def print_df():
+    print(hotel_trivago)
+
+def fix_month():
+    for i in range(len(hotel_trivago)):
+        if hotel_trivago.iloc[i, 'arrival_date_month'] == np.nan:
+            if hotel_trivago.iloc[i, 'arrival_date_week_number'] == 31 and hotel_trivago.iloc[i, 'arrival_date_day_of_month'] < 2:
+                hotel_trivago.at[i, 'arrival_date_moth'] = 'August'
+                print(i)
+            else:
+                hotel_trivago.at[i, 'arrival_date_moth'] = 'July'
+                print(i)
+
+fix_year()
+fix_month()
+print_df()
